5d024ef6397b3264a3783a34630e54af93090766
Ebuka-Onyekwe <ebukaonyekwe@ymail.com>
2020-08-24 09:51:33 +0200

    formatting

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -11,7 +11,7 @@ export class HttpGetService {
   constructor(public http: HttpClient) { }
 
    getGames() {
-    return  this.http.get(AppValues.GamesUrl);
+    return this.http.get(AppValues.GamesUrl);
   }
 
 }
